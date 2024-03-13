fc07ea41f5325c42e988162b2b5f7afb87addd76
Gabriel Rocha <bielsreal@icloud.com>
2024-03-13 15:00:29 -0300

    att

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-// target the button using the data attribute we added earlier
-const button = document.querySelector("[data-theme-toggle]");
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-button.addEventListener("click", () => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
-  // update the button text
-  const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-  button.innerText = newCta;
-
-  // update theme attribute on HTML
-  document.documentElement.setAttribute("data-theme", newTheme);
-
-  // update localStorage value
-  localStorage.setItem("theme", newTheme);
-});
-
-// get theme on page load
-const localStorageTheme = localStorage.getItem("theme");
-
-// set theme on button press
-localStorage.setItem("theme", newTheme);
-
-// calculate the current theme setting
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
-
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
-
-  return "light";
 }
-
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
